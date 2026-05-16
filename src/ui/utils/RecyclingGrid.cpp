b8d4cf413c36e03c5d9207bf1567701ec7582a83
beiklive <1376896787@qq.com>
2026-05-16 19:48:40 +0800

    fix: onNextPage触发改为滚动到内容50%位置，而非触底

--- src/ui/utils/RecyclingGrid.cpp
+++ src/ui/utils/RecyclingGrid.cpp
@@ -338,7 +338,7 @@ void RecyclingGrid::draw(NVGcontext* vg, float x, float y, float w, float h,
         if (contentH > frameH)
         {
             float off = getContentOffsetY();
-            if (off >= contentH - frameH - 20.0f)
+            if (off >= contentH * 0.5f)
             {
                 if (!m_requestNextPage && m_nextPageCallback)
                 {
