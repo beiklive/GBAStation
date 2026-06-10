f74852800c13348dc96ce92589103eff7d7cb1d5
beiklive <1376896787@qq.com>
2026-06-10 08:58:52 +0800

    5

--- src/emulator/CoreGenesis.cpp
+++ src/emulator/CoreGenesis.cpp
@@ -13,9 +13,7 @@ bool CoreGenesis::SetupGame(beiklive::GameEntry GameEntry)
     m_gameEntry = std::move(GameEntry);
     if (_loadCore())
     {
-#ifndef __SWITCH__
         _initConfig();
-#endif
         if (_loadRom(m_gameEntry.path))
         {
             _loadSram();
